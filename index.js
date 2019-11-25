e2c3d4aad55dcc2d545c3cf2f2e0757e647fc28e
davide <d.bulbarelli@aliaslab.net>
2019-11-25 15:13:30 +0100

    ref #9998

--- index.js
+++ index.js
@@ -2,60 +2,158 @@ const fs = require('fs');
 const http = require('http');
 
 const PORT = 9001;
-const FILE = 'versions.json';
+const DIR = 'projects';
 
-let rawdata = fs.readFileSync(FILE);
-let repoVersions = JSON.parse(rawdata);
+let repoVersions = new Map();
+
+fs.readdirSync(DIR).forEach(file => {
+    let rawdata = fs.readFileSync(`${DIR}/${file}`);
+    repoVersions.set(file.split('.')[0], JSON.parse(rawdata));
+});
 
 const server = http.createServer((req, res) => {
+    const pathParams = req.url.split('/');
+    const repoNames = Array.from(repoVersions.keys());
+    if (pathParams[2] == "versions" && pathParams[3] == "last" && pathParams.length == 4 && req.method === 'GET') {
+        const name = req.url.replace('/versions/last', '').substr(1);
+        if (name == undefined) {
+            res.statusCode = 400;
+            res.end('Bad request');
+        }
+        if (repoVersions.has(name)) {
+            const repoFound = repoVersions.get(name);
+            const lastVersion = JSON.stringify(repoFound[0]);
 
-    if (req.url.indexOf('/versions') != -1 && req.method === 'GET') {
-        const name = req.url.replace('/versions/', '');
+            res.statusCode = 200;
+            res.end(lastVersion);
+        }
+        else {
+            res.statusCode = 404;
+            res.end('Not found');
+        }
+    } else if (pathParams[2] == "versions" && pathParams.length == 3 && req.method === 'GET') {
+        const name = req.url.replace('/versions', '').substr(1);
+        if (name == undefined) {
+            res.statusCode = 400;
+            res.end('Bad request');
+        }
+        if (repoVersions.has(name)) {
+            let repoFound = repoVersions.get(name);
+
+            const lastVersion = JSON.stringify(Object.keys(repoFound.versions)[0]) + ":" + JSON.stringify(Object.values(repoFound.versions)[0])
+
+            res.statusCode = 200;
+            res.end(lastVersion);
+        }
+        else {
+            res.statusCode = 404;
+            res.end('Not found');
+        }
+
+    } else if (pathParams[2] == "versions" && pathParams.length == 3 && req.method === 'PUT') {
+        const name = req.url.replace('/versions', '').substr(1);
+
         if (name == undefined) {
             res.statusCode = 400;
             res.end('Bad request');
         }
 
-        const index = repoVersions.findIndex(repo => repo.name == name);
-        if (index == -1) {
-            res.statusCode = 404;
-            res.end('Not found');
-        }
-        else {
-            res.statusCode = 200;
-            res.end(repoVersions[index].version.toString());
-        }
-    }
-
-    if (req.url.indexOf('/versions') != -1 && req.method === 'POST') {
-        const name = req.url.replace('/versions/', '');
         let body = "";
         req.on('data', function (chunk) {
             body += chunk;
         });
 
         req.on('end', function () {
-            const indexEquals = body.indexOf('=');
-            const newVersion = body.substring(indexEquals + 1, body.length);
-            if (name == undefined || newVersion == undefined || newVersion == "") {
-                res.statusCode = 400;
-                res.end("Bad request");
-            }
+            if (repoVersions.has(name)) {
+                const newVersion = JSON.parse(body);
+                let repoFound = repoVersions.get(name);
+                if (repoFound[0].version == newVersion.version) {
+                    res.statusCode = 400;
+                    res.end("Bad request, version already present");
+                }
+                else {
+                    repoFound.unshift(newVersion);
 
-            const index = repoVersions.findIndex(repo => repo.name == name);
-            if (index == -1) {
-                res.statusCode = 404;
-                res.end("Not found");
+                    res.statusCode = 200;
+                    res.end(JSON.stringify(repoFound));
+
+                    const data = JSON.stringify(repoFound);
+                    fs.writeFileSync(`./projects/${name}.json`, data);
+                }
             }
             else {
-                repoVersions[index].version = new String(newVersion);
-                res.statusCode = 200;
-                res.end("OK");
+                res.statusCode = 404;
+                res.end('Not found');
             }
-
-            const data = JSON.stringify(repoVersions);
-            fs.writeFileSync(FILE, data);
         });
+    } else if (repoNames.some((name) => name == pathParams[1]) && pathParams.length == 2 && req.method === 'GET') {
+        res.writeHead(200, {
+            'Content-Type': 'text/html'
+        });
+        const repoSearched = repoVersions.get(pathParams[1]);
+
+        let resContent = "<html><body>";
+        resContent += `<h3>${pathParams[1]}</h3>`;
+        resContent += `<ul>`;
+        repoSearched.forEach((details) => {
+            resContent += `<li>Version <a href="#">${details.version}</a></li>`;
+            resContent += `<ul>`
+            details.tasks.forEach((task) => {
+                resContent += `<li><a href='https://redmine.aliaslab.net/issues/${task.id}'>Task ${task.id}</a></li>`;
+                resContent += `<ul>`;
+                task.messages.forEach((msg) => {
+                    resContent += `<li>${msg}</li>`
+                })
+                resContent += `</ul>`;
+            })
+            resContent += `</ul>`;
+        })
+        resContent += `</ul>`;
+
+        resContent += "</body></html>";
+        res.write(resContent);
+        res.end();
+    } else if (repoNames.some((name) => name == pathParams[1]) && pathParams.length == 3 && req.method === 'GET') {
+
+        const repoSearched = repoVersions.get(pathParams[1]);
+        if (repoSearched == undefined) {
+            res.statusCode = 404;
+            res.end('Not found, repository does not exist');
+            return;
+        }
+        const versionIndex = repoSearched.findIndex((entry) => entry.version == pathParams[2]);
+        if (versionIndex == -1) {
+            res.statusCode = 404;
+            res.end('Not found, version does not exist');
+            return;
+        }
+
+        res.writeHead(200, {
+            'Content-Type': 'text/html'
+        });
+        
+        let resContent = "<html><body>";
+        resContent += `<h3>${pathParams[1]}</h3>`;
+
+        resContent += `<b>Version ${repoSearched[versionIndex].version}</b>`;
+        resContent += `<ul>`
+        repoSearched[versionIndex].tasks.forEach((task) => {
+            resContent += `<li><a href='https://redmine.aliaslab.net/issues/${task.id}'>Task ${task.id}</a></li>`;
+            resContent += `<ul>`;
+            task.messages.forEach((msg) => {
+                resContent += `<li>${msg}</li>`
+            })
+            resContent += `</ul>`;
+        })
+        resContent += `</ul>`;
+
+        resContent += "</body></html>";
+        res.write(resContent);
+        res.end();
+
+    } else {
+        res.statusCode = 404;
+        res.end("Not found");
     }
 });
 
